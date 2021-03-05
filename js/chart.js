09d602145e5639fad839943adb4616d204b42d6a
squinnie <sarahq@kainos.com>
2021-03-05 19:04:45 +0000

    Revert "chart"
    This reverts commit ee5f9647cda10451872fdbe3fedf0018a60b157a.

--- js/chart.js
+++ js/chart.js
@@ -8,27 +8,27 @@ google.charts.setOnLoadCallback(drawChart);
 // instantiates the pie chart, passes in the data and
 // draws it.
 function drawChart() {
-  var data = google.visualization.arrayToDataTable([
-    ['Year', 'Sales', 'Expenses', 'Profit'],
-    ['2014', 1000, 400, 200],
-    ['2015', 1170, 460, 250],
-    ['2016', 660, 1120, 300],
-    ['2017', 1030, 540, 350]
+
+  // Create the data table.
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Topping');
+  data.addColumn('number', 'Slices');
+  data.addRows([
+    ['Mushrooms', 3],
+    ['Onions', 1],
+    ['Olives', 1],
+    ['Zucchini', 1],
+    ['Pepperoni', 2]
   ]);
 
-  var options = {
-    chart: {
-      title: 'Company Performance',
-      subtitle: 'Sales, Expenses, and Profit: 2014-2017',
-    }
-  };
+  // Set chart options
+  var options = {'title':'How Much Pizza I Ate Last Night',
+                 'width':350,
+                 'height':300};
 
-  var chart = new google.charts.Bar(document.getElementById('chart_div'));
-  chart.draw(data, google.charts.Bar.convertOptions(options));
-  
   // Instantiate and draw our chart, passing in some options.
-  // var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-  // chart.draw(data, options);
+  var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+  chart.draw(data, options);
 }
 
 document.addEventListener('DOMContentLoaded', function() {
