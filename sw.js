c5ee5b54a3ae1abe9a7fe36b3c3d10fa4c4fa799
squinnie <sarahq@kainos.com>
2021-03-20 18:46:49 +0000

    removed unnecessary files, updated cached pages and removed unused css file

--- sw.js
+++ sw.js
@@ -2,8 +2,29 @@ var cacheName = 'hello-pwa';
 var filesToCache = [
   '/',
   '/index.html',
-  '/css/style.css',
-  '/js/main.js'
+  '/chart.html',
+  '/log_history.html',
+  '/about.html',
+  '/valence_arousal.html',
+  '/manifest.json',
+  '/favicon.ico',
+  '/apple-touch-icon.png',
+  '/media/app_logo_192.png',
+  '/media/app_logo_512.png',  
+  '/media/model_of_affect.png',
+  '/js/chart.js',
+  '/js/index.js',
+  '/js/log_history.js',
+  '/js/main.js',
+  '/js/materialize.js',
+  '/js/materialize.min.js',
+  '/js/valence_arousal.js',
+  '/css/chart.css',
+  '/css/index.css',
+  '/css/log_history.css',
+  '/css/materialize.css',
+  '/css/materialize.min.css',
+  '/css/valence_arousal.css'
 ];
 
 /* Start the service worker and cache all of the app's content */
